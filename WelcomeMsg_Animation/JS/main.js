a6887eb1b664ca38d3c123102ccaf513d6c9e9cc
SleepZz <113423335+KarolisSleep@users.noreply.github.com>
2023-02-06 23:20:50 -0800

    Add files via upload

---
+++ WelcomeMsg_Animation/JS/main.js
@@ -0,0 +1,8 @@
+/* var welcomeMsg = document.getElementById("welcome-msg");
+
+welcomeMsg.onclick = function () {
+    welcomeMsg.addEventListener("welcome-msg"), () => {
+        welcomeMsg.className.add("on-screen");
+    }
+}; */
+
